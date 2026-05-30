6a17729593465a707df52b839e2153b579312e42
obada <top1obada@gmail.com>
2026-05-30 03:30:39 +0300

    Final fix for telephony library with complete replacement

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-
+import 'package:path_provider/path_provider.dart';
 import 'package:telephony/telephony.dart';
 
 final Telephony telephony = Telephony.instance;
